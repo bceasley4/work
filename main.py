5db3b5b6e07764df85a2da1080c30fde9f2451b9
bceasley4 <bceasley@calpoly.edu>
2023-06-03 18:58:38 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+def main():
+    print("Master branch")
+    print("\n1\n")
+    print(sys.argv[1])
+    print("\n2\n")
+    print(sys.argv[2])
+    print("\n3\n")
+    print(sys.argv[3])
+    print("\n4\n")
+    print(sys.argv[4])
+
+if __name__=="__main__":
+    main()
--- readfiles.sh
+++ readfiles.sh
@@ -1,1 +1,48 @@
+#!/bin/bash
+
+# Specify the two branches for comparison
+branch1="branch1"
+
+# Perform a git checkout of branch1
+git checkout $branch1
+
+# Get the merge base commit hash
+merge_base=$(git merge-base origin/master $branch)
+
+# Perform the git diff and store the different files into a variable
+changed_files=$(git diff --name-only $merge_base $branch)
+
+# Initialize arrays to store the lines and file names
+lines_branch1=()
+file_names_branch1=()
+
+# Iterate over each changed file
+for file in $changed_files; do
+  # Read the lines of the file into a temporary array
+  mapfile -t lines < $file
+
+  # Add the lines to the list and file names
+  lines_branch1+=("${lines[@]}")
+  file_names_branch1+=("$file")
+done
+
+# Perform a git checkout of branch2
+git checkout $branch2
+
+# Initialize arrays to store the lines and file names
+lines_branch2=()
+file_names_branch2=()
+
+# Iterate over each changed file
+for file in $changed_files; do
+  # Read the lines of the file into a temporary array
+  mapfile -t lines < $file
+
+  # Add the lines to the list and file names
+  lines_branch2+=("${lines[@]}")
+  file_names_branch2+=("$file")
+done
+
+# Pass the lists and file names to a Python file
+python3 main.py "${lines_branch1[@]}" "${lines_branch2[@]}" "${file_names_branch1[@]}" "${file_names_branch2[@]}"
 
